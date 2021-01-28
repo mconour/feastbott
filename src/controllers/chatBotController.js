2590a8776a1be04fa7849e025a1f74365cbeb90d
mconour <michaelconour@gmail.com>
2021-01-27 21:10:50 -0800

    Update handlePostback function, which handles messaging events

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -91,29 +91,28 @@ function handleMessage(sender_psid, received_message) {
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
             "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "generic",
-                "elements": [{
-                  "title": "Is this the right picture?",
-                  "subtitle": "Tap a button to answer.",
-                  "image_url": attachment_url,
-                  "buttons": [
-                    {
-                      "type": "postback",
-                      "title": "Yes!",
-                      "payload": "yes",
-                    },
-                    {
-                      "type": "postback",
-                      "title": "No!",
-                      "payload": "no",
-                    }
-                  ],
-                }]
-              }
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [{
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
             }
-          }
+        }
     }
 
     // Sends the response message
@@ -122,7 +121,23 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = {
+            "text": "Thanks!"
+        }
+    } else if (payload === 'no') {
+        response = {
+            "text": "Oops, try sending another image."
+        }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
